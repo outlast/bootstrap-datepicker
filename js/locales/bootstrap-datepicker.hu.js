6560fe202d9a0280bec430253622369d53a124c6
Aron Budinszky <aron@outlast.hu>
2013-05-21 22:35:18 +0200

    Adding sk and hu translations.

---
+++ js/locales/bootstrap-datepicker.hu.js
@@ -0,0 +1,13 @@
+/**
+ * Hungarian translation for bootstrap-datepicker
+ * Aron Budinszky <aron@outlast.hu>
+ */
+;(function($){
+	$.fn.datepicker.dates['hu'] = {
+		days: ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"],
+		daysShort: ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo", "Vas"],
+		daysMin: ["Va", "H", "K", "Sze", "Cs", "P", "Szo", "Va"],
+		months: ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augustus", "Szeptember", "Október", "November", "December"],
+		monthsShort: ["Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szept", "Okt", "Nov", "Dec"]
+	};
+}(jQuery));
---
+++ js/locales/bootstrap-datepicker.sk.js
@@ -0,0 +1,15 @@
+/**
+ * Slovak translation for bootstrap-datepicker
+ * Aron Budinszky <aron@outlast.hu>
+ * @todo It's probably right but someone should verify this.
+ */
+;(function($){
+	$.fn.datepicker.dates['sk'] = {
+		days: ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok", "Sobota", "Nedeľa"],
+		daysShort: ["Ne", "Po", "Ut", "St", "Št", "Pi", "So", "Ne"],
+		daysMin: ["Ne", "Po", "Ut", "St", "Št", "Pi", "So", "Ne"],
+		months: ["Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December"],
+		monthsShort: ["Jan", "Febr", "Mar", "Apr", "Máj", "Jún", "Júl", "Aug", "Sept", "Okt", "Nov", "Dec"]
+
+	};
+}(jQuery));
